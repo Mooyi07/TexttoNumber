f85ba7caa041fd91c3549351204a7c33c105414a
Mooyi07 <ljt.villanueva@gmail.com>
2025-03-26 10:45:25 +0800

    Simplified version

--- Main.java
+++ Main.java
@@ -3,51 +3,44 @@ import java.util.Scanner;
 
 public class Main {
     static int numRet(String input) {
-        // up to million only since it is INTEGER data type
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", 
-                          "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", 
-                          "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", 
-                          "eighty", "ninety", "hundred", "thousand", "million"};
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1000, 1000000};
-        
-        for (int i = 0; i < words.length; i++)
+        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                            "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+                            "eighty", "ninety", "hundred", "thousand", "million"};
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 
+                        70, 80, 90, 100, 1000, 1000000};
+
+        for (int i = 0; i < words.length; i++) {
             if (words[i].equals(input)) return values[i];
-        
+        }
         return 0;
     }
 
-    static int outputValue(String input){
-        // Checking if the input has spaces, if so then splits into arrays.
-        int output = 0;
-        if (!input.contains(" ")){
-            output = numRet(input);
-        } else {
-            String[] myArray = input.split(" ");
-            
-            for (int i = 0; i < myArray.length; i++){
-                int returnVal = numRet(myArray[i]);
-                if (returnVal > 99){
-                    output = output + (numRet(myArray[i-1]) * numRet(myArray[i])); 
-                }
-                // Checks the last 2 arrays if they are the by Tens or Ones
-                if ((myArray.length-2) <= i && returnVal < 91){
-
-                    // Instead of multiplying the values, it will be added instead
-                    output = output + numRet(myArray[i]);
-                }
+    static int outputValue(String input) {
+        String[] words = input.split(" ");
+        int total = 0, temp = 0;
+        
+        for (String word : words) {
+            int num = numRet(word);
+            if (num == 100 || num == 1000 || num == 1000000) {
+                temp *= num;
+            } else {
+                temp += num;
+            }
+            if (num >= 1000) {
+                total += temp;
+                temp = 0;
             }
         }
-        return output;
+        return total + temp;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input: ");
-
-        // prevention of uppercase formats
         String input = scanner.nextLine().toLowerCase();
-
         System.out.println("Output: " + String.format("%,d", outputValue(input)));
         scanner.close();
     }
+
 }
