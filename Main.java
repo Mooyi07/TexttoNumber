0df748fb907cb25a0bb6496ee5b9044e07ec153f
Lanz Joseph Tarrosa Villanueva <120180277+Mooyi07@users.noreply.github.com>
2025-04-28 21:33:43 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,32 +2,47 @@
 import java.util.Scanner;
 
 public class Main {
-    static int numRet(String input) {
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-                            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-                            "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
-                            "eighty", "ninety", "hundred", "thousand", "million"};
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 
-                        70, 80, 90, 100, 1000, 1000000};
 
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(input)) return values[i];
-        }
-        return 0;
+    private static final String[] WORDS = {
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+        "eighty", "ninety", "hundred", "thousand", "million"
+    };
+
+    private static final int[] VALUES = {
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        30, 40, 50, 60, 70, 80, 90, 100, 1000, 1000000
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        int output = parseTextToNumber(input);
+        System.out.println("Output: " + String.format("%,d", output));
+
+        scanner.close();
     }
 
-    static int outputValue(String input) {
+    private static int parseTextToNumber(String input) {
         String[] words = input.split(" ");
-        int total = 0, temp = 0;
-        
+        int total = 0;
+        int temp = 0;
+
         for (String word : words) {
-            int num = numRet(word);
-            if (num == 100 || num == 1000 || num == 1000000) {
-                temp *= num;
+            int value = wordToNumber(word);
+
+            if (value == 100 || value == 1000 || value == 1000000) {
+                temp *= value;
             } else {
-                temp += num;
+                temp += value;
             }
-            if (num >= 1000) {
+
+            if (value >= 1000) {
                 total += temp;
                 temp = 0;
             }
@@ -35,12 +50,12 @@ public class Main {
         return total + temp;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input: ");
-        String input = scanner.nextLine().toLowerCase();
-        System.out.println("Output: " + String.format("%,d", outputValue(input)));
-        scanner.close();
+    private static int wordToNumber(String word) {
+        for (int i = 0; i < WORDS.length; i++) {
+            if (WORDS[i].equals(word)) {
+                return VALUES[i];
+            }
+        }
+        return 0;
     }
-
 }
